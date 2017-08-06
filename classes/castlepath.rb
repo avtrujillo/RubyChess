9efa80e1de2ac9b3726b13438f704540c6195b89
Alexander V Trujillo <alexandervtrujillo@gmail.com>
2017-08-06 16:03:46 -0500

    minor changes to castlepath.rb

--- classes/castlepath.rb
+++ classes/castlepath.rb
@@ -1,5 +1,6 @@
-class CastlePath # note: not a subclass of Path
-	attr_accessor :rook, :tiles, :king_tiles, :king, :rook_tiles,
+# note: not a subclass of Path
+class CastlePath
+	attr_reader :rook, :tiles, :king_tiles, :king, :rook_tiles,
 	:rook_dest, :king_dest
 	def initialize(rook)
 		@rook = rook
@@ -7,40 +8,48 @@ class CastlePath # note: not a subclass of Path
 		@tiles = @rook.simlevel.tiles
 		@rook_tiles = []
 		@king_tiles = []
-		self.find_rook_path
-		self.find_rook_path
-		@rook_dest = @rook_tiles.last if @rook_tiles
-		@king_dest = @king_tiles.last if @king_tiles
+		find_king_path
+		find_rook_path
+		@rook_dest = @rook_tiles.last
+		@king_dest = @king_tiles.last
 	end
+
+	ROOK_PATH_HASH = {
+		'A1' => ['B1', 'C1', 'D1'], 'A8' => ['B8', 'C8', 'D8'],
+		'H1' => ['G1', 'F1'], 'H8' => ['G8', 'F8']
+	}
+
 	def find_rook_path
-		rook_path_hash = {
-			"A1" => ["B1", "C1" "D1"], "A8" => ["B8", "C8" "D8"],
-			"H1" => ["G1", "F1"], "H8" => ["G8", "F8"]
-		}
-		tiles = self.rook.simlevel.tiles
-		rook_path_hash[self.rook.tile.coordinates.name.capitalize].each do |coor|
-			tile = tiles.find {|tile| tile.coordinates.name == coor}
-			self.rook_tiles.push(tile) if tile
+		tiles = @rook.simlevel.tiles
+		ROOK_PATH_HASH[@rook.tile.coordinates.name.capitalize].each do |coor|
+			tile = @tiles.find {|t| t.coordinates.name == coor}
+			@rook_tiles << tile if tile
 		end
 	end
+
+	KING_PATH_HASH = {
+		'A1' => ['E1', 'D1' 'C1'], 'A8' => ['E8', 'D8' 'C8'],
+		'H1' => ['E1', 'F1', 'G1'], 'H8' => ['E8', 'F8', 'G1']
+	}
+
 	def find_king_path
-		king_path_hash = {
-			"A1" => ["E1", "D1" "C1"], "A8" => ["E8", "D8" "C8"],
-			"H1" => ["E1", "F1", "G1"], "H8" => ["E8", "F8", "G1"]
-		}
-		king_path_hash[self.rook.tile.name.capitalize].each do |coor|
-			tile = tiles.find {|tile| tile.coordinates.name == coor}
-			self.king_tiles.push(tile)
+		KING_PATH_HASH[self.rook.tile.name.capitalize].each do |coor|
+			tile = @tiles.find {|t| t.coordinates.name == coor}
+			@king_tiles << tile
 		end
 	end
+
 	def clear?
-		if self.rook_tiles.is_a?(Array) && !self.rook_tiles.empty?
-			!(self.rook_tiles.any? {|tile| tile.occupied_piece})
+		!@king_tiles.empty? &&
+		@king_tiles.none? {|tile| tile.occupied_piece} &&
+		!@rook_tiles.empty? &&
+		@rook_tiles.none? {|tile| tile.occupied_piece}
+	end
+
+	def safe?
+		@king_tiles.none? do |tile|
+			Move.new(@.rook.game, @king.tile, tile).ally_check
 		end
 	end
-	def safe?
-		self.king_tiles.any? {|tile|
-			Move.new(self.rook.game, king.tile, tile).ally_check
-		}
-	end
+
 end
